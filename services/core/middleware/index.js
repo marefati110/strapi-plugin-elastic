115ad85dcd0026e69c924545ed597b390c869ef2
ali <marefati110@gmail.com>
2020-09-21 14:54:44 +0330

    cleanup

--- services/core/middleware/index.js
+++ services/core/middleware/index.js
@@ -23,52 +23,51 @@ function checkRequest(ctx) {
   return status;
 }
 
-const elasticsearchManager = async (ctx) => {
-  // validation
-  if (!checkRequest(ctx)) return;
-  // extract data from url
-  const url = ctx.request.url,
-    body = ctx.response.body,
-    apiName = url.match(urlIndexPattern) || [],
-    urlID = url.match(urlIdPattern) || [],
-    adminApiName = url.match(adminUrlIndexPattern) || [];
+module.exports = {
+  elasticsearchManager: async (ctx) => {
+    // validation
+    if (!checkRequest(ctx)) return;
+    // extract data from url
+    const url = ctx.request.url,
+      body = ctx.response.body,
+      apiName = url.match(urlIndexPattern) || [],
+      urlID = url.match(urlIdPattern) || [],
+      adminApiName = url.match(adminUrlIndexPattern) || [];
 
-  const index_ = urls[apiName[1]] || adminUrls[adminApiName[1]] || false;
+    const index_ = urls[apiName[1]] || adminUrls[adminApiName[1]] || false;
 
-  if (!index_) return;
+    if (!index_) return;
 
-  // define main variables
-  const id = body.id || ctx.params.id || urlID[0],
-    index = index_.index,
-    withRelated = index_.withRelated;
-
-  if (ctx.request.method === 'POST' || ctx.request.method === 'PUT') {
-    let data;
-    if (setting.fillByResponse === true) {
-      data = body;
-      //
-    } else if (setting.fillByResponse === false && withRelated) {
-      const raw_data = await strapi
-        .query(index_.service)
-        .model.query((qb) => {
-          qb.where('id', '=', id);
-        })
-        .fetch({
-          withRelated: withRelated,
-        });
-      data = await raw_data.toJSON();
-
-      //
-    } else if (setting.fillByResponse === false && !withRelated) {
-      data = await strapi.services[index_.service].findOne({ id: id }, []);
-    }
-
-    return await createOrUpdate(index, id, data);
+    // define main variables
+    const id = body.id || ctx.params.id || urlID[0],
+      index = index_.index,
+      withRelated = index_.withRelated;
     //
-  }
+    if (ctx.request.method === 'POST' || ctx.request.method === 'PUT') {
+      let data;
+      if (setting.fillByResponse === true) {
+        data = body;
+        //
+      } else if (setting.fillByResponse === false && withRelated) {
+        //
+        const raw_data = await strapi
+          .query(index_.service)
+          .model.query((qb) => {
+            qb.where('id', '=', id);
+          })
+          .fetch({
+            withRelated: withRelated,
+          });
+        data = await raw_data.toJSON();
+        //
+      } else if (setting.fillByResponse === false && !withRelated) {
+        data = await strapi.services[index_.service].findOne({ id: id }, []);
+      }
 
-  // delete data
-  if (ctx.request.method === 'DELETE') return await destroy(index, id);
+      return await createOrUpdate(index, id, data);
+      //
+    }
+    // delete data
+    if (ctx.request.method === 'DELETE') return await destroy(index, id);
+  },
 };
-
-module.exports = { elasticsearchManager };
