a6315c3b37eacb53f0ba5926530fee0161a2deeb
ali <marefati110@gmail.com>
2020-10-04 21:46:44 +0330

    add find func to elastic object

---
+++ services/core/functions/find.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  find: async (index, query) => {
+    try {
+      const elasticsearchResponse = await strapi.elastic.search({
+        index,
+        ...query,
+      });
+      return elasticsearchResponse;
+    } catch (e) {
+      return;
+    }
+  },
+};
