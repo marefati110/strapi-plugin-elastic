61a3ed353d6fb50af118acfc83d3f3da6e9333b8
ali <marefati110@gmail.com>
2020-10-16 21:22:17 +0330

    create api for frontend

--- controllers/elasticsearch.js
+++ controllers/elasticsearch.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const { cron } = require('../services/core/migration');
 
 module.exports = {
@@ -17,5 +19,36 @@ module.exports = {
     const params = ctx.request.body;
     await strapi.elastic.migrateModel(params);
     await ctx.send('ok');
+  },
+
+  fetchModels: (ctx) => {
+    const { models } = strapi.config.elasticsearch;
+
+    const availableModels = models.filter((model) => model.enable === true);
+
+    const response = _.map(
+      availableModels,
+      _.partialRight(_.pick, ['model', 'plugin', 'index', 'migration', 'pk'])
+    );
+    return ctx.send(response);
+  },
+
+  fetchModel: async (ctx) => {
+    const { index, _start, _limit } = ctx.query;
+
+    const data = await strapi.elastic.search({
+      index,
+      size: _limit || 10,
+      from: _limit * _start || 0,
+      body: {
+        query: {
+          match_all: {},
+        },
+      },
+    });
+
+    if (data.statusCode !== 200) return ctx.badRequest();
+
+    return ctx.send({ data: data.body.hits.hits });
   },
 };
