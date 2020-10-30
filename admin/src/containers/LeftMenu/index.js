61e9b108724403a001cc08fe5556e438ece4a3eb
amir <jramir669@gmail.com>
2020-10-30 19:26:14 +0330

    add left menu and data view

---
+++ admin/src/containers/LeftMenu/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button } from '@buffetjs/core';
+import Wrapper from './Wrapper';
+import LeftMenuItem from '../../Components/LeftMenuItem';
+
+const LeftMenu = ({ models, activeModel, setActiveModel }) => {
+  return (
+    <Wrapper className="col-md-4 col-sm-12">
+      <h4>models</h4>
+      <hr />
+      {models && models.length
+        ? models.map((model) => (
+            <LeftMenuItem
+              label={model.index}
+              onClick={() => setActiveModel(model)}
+              active={model.index === activeModel?.index}
+            />
+          ))
+        : null}
+    </Wrapper>
+  );
+};
+
+LeftMenu.propTypes = {
+  models: PropTypes.array.isRequired,
+  activeModel: PropTypes.object.isRequired,
+  setActiveModel: PropTypes.func.isRequired,
+};
+
+export default LeftMenu;
