b373ea077a2812357b8cc7a701d2b02a22d76a2e
ali <marefati110@gmail.com>
2020-11-01 21:39:14 +0330

    move all functions to index.js

--- services/core/functions/index.js
+++ services/core/functions/index.js
@@ -1,8 +1,58 @@
-const { createOrUpdate } = require('./CreateOrUpdate');
-const { findOne } = require('./findOne');
-const { find } = require('./find');
-const { destroy } = require('./Delete');
-
+/* eslint-disable no-empty */
 module.exports = {
-  createOrUpdate, findOne, find, destroy,
+  find: async (index, query) => {
+    try {
+      const elasticsearchResponse = await strapi.elastic.search({
+        index,
+        ...query,
+      });
+      return elasticsearchResponse;
+    } catch (e) {}
+  },
+  findOne: async (index, id) => {
+    try {
+      const elasticsearchResponse = await strapi.elastic.get({
+        index,
+        id,
+      });
+      return elasticsearchResponse;
+    } catch (e) {}
+  },
+  destroy: async (index, id) => {
+    let elasticsearchResponse;
+    try {
+      elasticsearchResponse = await strapi.elastic.delete({
+        id,
+        index,
+      });
+    } catch (e) {
+      console.error(e.message);
+    }
+
+    if (elasticsearchResponse) {
+      return true;
+    }
+    return false;
+  },
+  createOrUpdate: async (index, id, data) => {
+    let elasticsearchResponse;
+
+    try {
+      elasticsearchResponse = await strapi.elastic.update({
+        id,
+        index,
+        body: {
+          doc: data,
+          doc_as_upsert: true,
+        },
+      });
+    } catch (e) {
+      strapi.log.error(e.message);
+    }
+
+    if (elasticsearchResponse) {
+      return true;
+    }
+    return false;
+  },
 };
